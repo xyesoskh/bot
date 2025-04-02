3abdf84c91d2a6616225d91f14f14c03d03bd8e2
xyesoskh <azamatdul112233@gmail.com>
2025-04-02 16:17:38 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,6 @@ def handle_message(message):
     # Отправляем стандартное сообщение
     if message.chat.id != ADMIN_ID:
         bot.send_message(message.chat.id, "✅ Ваше сообщение получено! Ожидайте ответа от администратора.")
-     )
     user_messages[msg.message_id] = message.chat.id  # Запоминаем, кому ответить
 
 # Обрабатываем ответы админа
